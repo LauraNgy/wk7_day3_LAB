c2ddc36239d676151068084ed09c368c18ba9eb3
Laura Nagy <ngylaura@gmail.com>
2018-09-12 15:05:39 +0100

    changes country display DOM

--- src/views/country_info.js
+++ src/views/country_info.js
@@ -12,11 +12,11 @@ CountryInfo.prototype.bindEvents = function () {
     this.element.innerHTML = "";
     const country = event.detail;
     const countryName = new CreateAppend("h2", country.name, parent)
-    const countryRegion = new CreateAppend("h3", country.region, parent)
     const countryFlag = new Image(200, 150)
     countryFlag.src = country.flag
     parent.appendChild(countryFlag);
-    const countryUl = new CreateAppend("ul", "", parent);
+    const countryRegion = new CreateAppend("h3", `Region: ${country.region}`, parent)
+    const countryUl = new CreateAppend("ul", "Languages:", parent);
     const countryLanguages = country.languages;
     countryLanguages.forEach((language) => {
       const countryLanguage = new CreateAppend("li", language.name, countryUl)
